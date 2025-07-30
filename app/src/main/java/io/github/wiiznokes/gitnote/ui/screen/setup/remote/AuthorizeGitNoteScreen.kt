f603736531b3e5372defe9816870ba313bde7359
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2025-07-30 11:46:40 +0200

    AuthorizeGitNoteScreen

--- app/src/main/java/io/github/wiiznokes/gitnote/ui/screen/setup/remote/AuthorizeGitNoteScreen.kt
+++ app/src/main/java/io/github/wiiznokes/gitnote/ui/screen/setup/remote/AuthorizeGitNoteScreen.kt
@@ -1,5 +1,6 @@
 package io.github.wiiznokes.gitnote.ui.screen.setup.remote
 
+import android.content.Intent
 import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.material3.Button
@@ -9,7 +10,10 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import io.github.wiiznokes.gitnote.ui.component.AppPage
+import io.github.wiiznokes.gitnote.ui.screen.setup.NewRepoMethodScreen
+import io.github.wiiznokes.gitnote.ui.viewmodel.InitState
 import io.github.wiiznokes.gitnote.ui.viewmodel.InitState.AuthState
 import io.github.wiiznokes.gitnote.ui.viewmodel.SetupViewModel
 
@@ -18,11 +22,11 @@ private const val TAG = "AuthorizeGitNoteScreen"
 @Composable
 fun AuthorizeGitNoteScreen(
     onBackClick: () -> Unit,
-    vm: SetupViewModel,
+    authState: InitState,
     onSuccess: () -> Unit,
+    getLaunchOAuthScreenIntent: () -> Intent,
+    vmHashCode: Int,
 ) {
-
-    val authState = vm.initState.collectAsState().value
 
     AppPage(
         title = "Authorize Gitnote",
@@ -31,18 +35,19 @@ fun AuthorizeGitNoteScreen(
         onBackClick = onBackClick,
         onBackClickEnabled = authState.isClickable()
     ) {
-        val ctx = LocalContext.current
 
         LaunchedEffect(authState) {
-            Log.d(TAG, "LaunchedEffect: $authState, hash=${vm.hashCode()}")
+            Log.d(TAG, "LaunchedEffect: $authState, hash=${vmHashCode}")
             if (authState is AuthState.Success) {
                 onSuccess()
             }
         }
 
+        val ctx = LocalContext.current
+
         Button(
             onClick = {
-                val intent = vm.getLaunchOAuthScreenIntent()
+                val intent = getLaunchOAuthScreenIntent()
                 ctx.startActivity(intent)
             },
             enabled = authState.isClickable() && authState != AuthState.Success
@@ -54,4 +59,19 @@ fun AuthorizeGitNoteScreen(
             }
         }
     }
+}
+
+@Preview
+@Composable
+private fun AuthorizeGitNoteScreenPreview() {
+
+    AuthorizeGitNoteScreen(
+        onBackClick = {},
+        authState = AuthState.Idle,
+        onSuccess = {},
+        getLaunchOAuthScreenIntent = {
+            Intent()
+        },
+        vmHashCode = 0
+    )
 }
