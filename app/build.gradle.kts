9bbef99a1bfc1531747fcde27fd89b895fbabf48
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2025-09-25 19:18:27 +0200

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "io.github.wiiznokes.gitnote"
         minSdk = 30
         targetSdk = 36
-        versionCode = 9
-        versionName = "25.08.1"
+        versionCode = 10
+        versionName = "25.09"
 
         buildConfigField(
             "String",
