203dbfa60222dc3025d706bb91035073bfa3d4cf
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2025-11-14 14:18:41 +0100

    new release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "io.github.wiiznokes.gitnote"
         minSdk = 30
         targetSdk = 36
-        versionCode = 11
-        versionName = "25.11"
+        versionCode = 12
+        versionName = "25.11.1"
 
         buildConfigField(
             "String",
