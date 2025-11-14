841e2d98460a5c69826ba71840e57f53d375f213
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2025-11-14 17:49:11 +0100

    init

--- app/src/main/rust/src/mime_types.rs
+++ app/src/main/rust/src/mime_types.rs
@@ -1,14 +1,11 @@
-use mime_guess::mime;
-
-const ADDITIONAL_SUPPORTED_EXTENSIONS: &[&str] = &["sh", "fish", "ps1", "bat"];
+use include_lines::include_lines;
 
 pub fn is_extension_supported(extension: &str) -> bool {
-    if ADDITIONAL_SUPPORTED_EXTENSIONS.contains(&extension) {
-        return true;
-    }
+    let text_extensions = include_lines!("./extensions/text.txt");
+    let markdown_extensions = include_lines!("./extensions/markdown.txt");
 
-    let mimes = mime_guess::from_ext(extension);
-    mimes.iter().any(|mime| mime.type_() == mime::TEXT)
+    text_extensions.binary_search(&extension).is_ok()
+        || markdown_extensions.binary_search(&extension).is_ok()
 }
 
 #[cfg(test)]
@@ -16,6 +13,13 @@ mod test {
     use std::{collections::HashMap, fs, path::Path};
 
     use super::*;
+
+    #[test]
+    fn test1() {
+        assert!(is_extension_supported("md"));
+        assert!(is_extension_supported("txt"));
+        assert!(!is_extension_supported("bin"));
+    }
 
     #[test]
     #[ignore = "local repo"]
@@ -43,27 +47,19 @@ mod test {
         let mut extensions = counts
             .into_iter()
             .map(|(extension, count)| {
-                let supported = {
-                    let mimes = mime_guess::from_ext(&extension);
-                    mimes.iter().any(|mime| mime.type_() == mime::TEXT)
-                };
+                let supported = is_extension_supported(&extension);
 
-                let supported_manually =
-                    ADDITIONAL_SUPPORTED_EXTENSIONS.contains(&extension.as_str());
-
-                (extension, count, supported, supported_manually)
+                (extension, count, supported)
             })
             .collect::<Vec<_>>();
 
         extensions.sort_by(|a, b| b.1.cmp(&a.1));
         extensions.sort_by(|a, b| b.2.cmp(&a.2));
 
-        println!("extension     supported     supported_manually    count");
+        println!("extension     supported     count");
         // Print results
-        for (ext, count, supported, supported_manually) in extensions {
-            println!(
-                "{ext:<10}    {supported:<5}         {supported_manually:<5}                 {count:<6}"
-            );
+        for (ext, count, supported) in extensions {
+            println!("{ext:<10}    {supported:<5}         {count:<6}");
         }
     }
 }
--- justfile
+++ justfile
@@ -1,9 +1,21 @@
 set windows-powershell := true
 
+main:
+    echo "do nothing"
+
 fix:
     ./gradlew lintFix
 
+fmt-just:
+    just --fmt --unstable
+
 prettier:
-	# install on Debian: sudo snap install node --classic
-	# npx is the command to run npm package, node is the runtime
-	npx prettier -w . --ignore-path ./.gitignore --ignore-path ./.prettierignore
+    # install on Debian: sudo snap install node --classic
+    # npx is the command to run npm package, node is the runtime
+    npx prettier -w . --ignore-path ./.gitignore --ignore-path ./.prettierignore
+
+sort-supported-extension:
+    #!/usr/bin/env bash
+    for f in $(ls app/src/main/rust/extensions 2>/dev/null); do
+    	sort app/src/main/rust/extensions/$f -o app/src/main/rust/extensions/$f
+    done
