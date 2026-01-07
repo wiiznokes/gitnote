2eb40fb09de56dfa6060429e4bebab425ecd0c42
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2026-01-07 13:46:16 +0100

    26.01.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "io.github.wiiznokes.gitnote"
         minSdk = 30
         targetSdk = 36
-        versionCode = 14
-        versionName = "26.01"
+        versionCode = 15
+        versionName = "26.01.1"
 
         buildConfigField(
             "String",
