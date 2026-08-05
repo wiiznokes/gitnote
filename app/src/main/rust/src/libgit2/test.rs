337721eb74b455c9488ec02cbc7f3aa311524407
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2026-08-05 03:08:30 +0200

    add bench

--- app/src/main/rust/src/libgit2/test.rs
+++ app/src/main/rust/src/libgit2/test.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, time::Instant};
+
 use super::*;
 
 #[test]
@@ -5,21 +7,43 @@ use super::*;
 fn timestamp() {
     open_repo("../../../../../repo_test").unwrap();
 
-    let res = get_timestamps();
+    let mut timestamps = HashMap::new();
 
-    dbg!(&res);
+    let now = Instant::now();
+
+    get_timestamps(|path, time| {
+        timestamps.insert(path.to_string(), time);
+        Ok(())
+    })
+    .unwrap();
+
+    let elapsed = now.elapsed();
+
+    println!("{elapsed:?}");
 }
 
+// cargo test timestamp2 --release -- --nocapture --ignored
 #[test]
 #[ignore = "local repo"]
 fn timestamp2() {
     open_repo("../../../../../note-pv").unwrap();
 
-    let res = get_timestamps();
+    let mut timestamps = HashMap::new();
+    let now = Instant::now();
 
-    let mut res = res.unwrap().into_iter().collect::<Vec<_>>();
+    get_timestamps(|path, time| {
+        timestamps.insert(path.to_string(), time);
+        Ok(())
+    })
+    .unwrap();
+
+    let mut res = timestamps.into_iter().collect::<Vec<_>>();
 
     res.sort_by(|a, b| a.1.cmp(&b.1));
 
     dbg!(&res);
+
+    let elapsed = now.elapsed();
+
+    println!("{elapsed:?}");
 }
